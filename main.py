b2c78ee17b02753bf5c4d4c7a232f4aae4e67cbd
Jeremy Bejarano <jmbejara@gmail.com>
2025-02-22 20:50:56 -0500

    Adjustments for Vercel

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = FastAPI()
 # Mount the "main" directory to serve static files
 app.mount("/", StaticFiles(directory="main", html=True), name="main")
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# Don't use this for Vercel
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
