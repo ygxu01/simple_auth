70e3b0c1a5f2d68c97a4ded5a4b94804cd4016fb
Jeremy Bejarano <jmbejara@gmail.com>
2025-02-22 21:36:41 -0500

    Example 4: Sphinx

--- ex4_sphinx/main.py
+++ ex4_sphinx/main.py
@@ -4,9 +4,27 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 # Mount the "main" directory to serve static files
-app.mount("/", StaticFiles(directory="main", html=True), name="main")
+app.mount("/", StaticFiles(directory="./ex4_sphinx/main", html=True), name="main")
 
 # Don't use this for Vercel
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# For this example, the vercel.json file should look like this:
+"""
+{
+    "builds": [
+        {
+            "src": "ex4_sphinx/main.py",
+            "use": "@vercel/python"
+        }
+    ],
+    "routes": [
+        {
+            "src": "/(.*)",
+            "dest": "ex4_sphinx/main.py"
+        }
+    ]
+}  
+"""
