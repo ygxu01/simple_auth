c56e66235bb3c75338e7244ef64c29cd2d3f25b5
Jeremy Bejarano <jmbejara@gmail.com>
2025-02-22 21:10:23 -0500

    Break up into examples

---
+++ ex2_templates/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates  
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+message = "Hello class, from the .py file"
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "message": message})
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ ex4_sphinx/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# Mount the "main" directory to serve static files
+app.mount("/", StaticFiles(directory="main", html=True), name="main")
+
+# Don't use this for Vercel
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
